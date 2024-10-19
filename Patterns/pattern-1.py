f509551d046a863a78ffa76816701cf83e6b83ca
__mr.prank <128305368+MuthuPandi-1963@users.noreply.github.com>
2024-10-19 13:05:27 +0530

    for loop patterns => pattern-1

---
+++ Patterns/pattern-1.py
@@ -0,0 +1,23 @@
+# input n=5
+
+""" output: 
+
+    *****   
+    *****
+    *****
+    *****
+    *****
+"""
+
+n=5
+
+#method -1
+for i in range(n):
+    star =""
+    for j in range(n):
+        star+="*"
+    print(star)
+print("    ")
+#method -2
+for i in range(n):
+    print("*"*n)
