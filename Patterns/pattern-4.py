8b2baad5fa9d93723f36a7336f02d39b13e9c51e
__mr.prank <128305368+MuthuPandi-1963@users.noreply.github.com>
2024-11-13 15:54:04 +0530

    pattern-4

--- Patterns/pattern-4.py
+++ Patterns/pattern-4.py
@@ -1,12 +1,49 @@
+# input n=5
+
+# Output 
+#  *********
+#  *       *
+#  *       *
+#  *       *
+#  *       *
+#  *       *
+#  *       *
+#  *       *
+#  *       *
+#  *       *
+#  *********
 n=5
-for i in range(2*n+1):
-    s=" "
-    for j in range(2*n-1):
-        if i==0 or i==10:
+# for i in range(2*n+1):
+#     s=" "
+#     for j in range(2*n-1):
+#         if i==0 or i==2*n:
+#             s+="*"
+#         elif (i>=1 and i<=2*n-1) and (j==0 or j==2*n-2):
+#             s+="*"
+#         elif j>=1 and j<=2*n-1:
+#             s+=" "
+#     print(s)
+
+for i in range(n):
+    s=""
+    for j in range(n):
+        if i==0 or i==n-1:
             s+="*"
-        elif (i>=1 and i<=9) and (j==0 or j==8):
+        elif (i>=1 and i<=n-1) and (j==0 or j==n-1):
             s+="*"
-        elif j>=1 and j<=7:
+        elif j>=1 and j<=n-2:
             s+=" "
     print(s)
+
      
+for i in range(n):
+    s="" 
+    for j in range(n):
+        if i==0 or i==n-1:
+            s+="*"
+        else:
+            if j==0 or j==n-1:
+                s+="*"
+            else:
+                s+=" "
+    print(s)
